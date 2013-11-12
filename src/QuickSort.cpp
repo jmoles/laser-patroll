ff2ee075e61551061006c3b8709198c8771d4579
Eric Krause <krause.ericm@gmail.com>
2013-11-11 21:07:01 -0800

    rough (read: untested) draft of quicksort added

---
+++ src/QuickSort.cpp
@@ -0,0 +1,44 @@
+#include "QuickSort.hpp"
+
+
+QuickSort::QuickSort() {
+	
+}
+
+QuickSort::~QuickSort() {
+
+}
+
+
+void QuickSort::Sort(DataType &data_in, int left, int right) {
+
+  int i = left, j = right;
+  int tmp;
+  int pivot = data_in[(left + right) / 2];
+ 
+  /* partition */
+  while (i <= j) 
+  {
+    while (data_in[i] < pivot)
+      i++;
+      
+    while (data_in[j] > pivot)
+      j--;   
+    
+    if (i <= j) 
+    {
+      tmp = data_in[i];
+      data_in[i] = data_in[j];
+      data_in[j] = tmp;
+      i++;
+      j--;
+    }
+  };
+ 
+  /* recursion */
+  if (left < j)
+    quickSort(data_in, left, j);
+  
+  if (i < right)
+    quickSort(data_in, i, right);
+}
