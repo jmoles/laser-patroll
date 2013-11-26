1ef057fd1ded35669da536f7b61eecb6cf8280c9
Eric Krause <krause.ericm@gmail.com>
2013-11-26 15:49:01 -0800

    broken mergesort code

--- src/MergeSort.cpp
+++ src/MergeSort.cpp
@@ -1,5 +1,5 @@
 #include "MergeSort.hpp"
-
+#include <math.h>       /* floor */
 
 MergeSort::MergeSort() {
 	
@@ -9,7 +9,111 @@ MergeSort::~MergeSort() {
 
 }
 
+void* MergeSort::Thread_MSort(void * args){
+  thread_args * a = (thread_args *) args;
 
-void MergeSort::Sort(DataType &data_in) {
-	std::sort(data_in.begin(), data_in.end());
+  MSort(a->threads_remaining, a->low, a->high);
+
 }
+
+
+void MergeSort::MSort(size_t threads_remaining, size_t low, size_t high){
+    
+    
+    if(low<high)
+    {
+        size_t pivot= (low+high)/2;
+        
+        if (threads_remaining == 0)
+        {
+            if(low<high)
+            {
+                MSort(0,low,pivot);
+                MSort(0,pivot+1,high);
+              
+                Merge(low,pivot,high);
+            }
+        }
+
+        else // threads_remaining > 1
+        {
+            // set up pthreads stuff
+            pthread_attr_t attr;
+            pthread_t thread;
+            pthread_attr_init (&attr);
+            pthread_attr_setscope (&attr,PTHREAD_SCOPE_SYSTEM);
+           
+            // subtract 1 from threads left (one will be allocated now)
+            // then divide the remainder between "children"
+            size_t lt, rt; // (left threads, right threads)
+            lt = rt = (threads_remaining -1)/2;
+            rt += (threads_remaining-1)%2; // remainder has to go somewhere!
+            
+            // set up args struct to pass to new thread
+            thread_args a;
+            a.threads_remaining = lt;
+            a.low = low;
+            a.high = pivot;
+            
+            // new thread does left, current thread does right, current waits on left, then merges
+            pthread_create(&thread, &attr, &MergeSort::Thread_MSort, (void *) &a);
+            MSort(rt, pivot+1, high); 
+            pthread_join(thread, NULL);
+            Merge(low,pivot,high);
+        }
+    }
+}
+
+void MergeSort::Merge(size_t low, size_t pivot, size_t high){
+    
+    size_t h,i,j,k;
+    h=low;
+    i=low;
+    j=pivot+1;
+      
+    while((h<=pivot)&&(j<=high))
+    {
+        if(src[h]<=src[j])
+        {
+            dst[i]=src[h];
+            h++;
+        }
+      
+        else
+        {
+            dst[i]=src[j];
+            j++;
+        }
+      
+        i++;
+    }
+
+    if(h>pivot)
+    {
+        for(k=j; k<=high; k++)
+        {
+            dst[i]=src[k];
+            i++;
+        }
+    }
+    
+    else
+    {
+        for(k=h; k<=pivot; k++)
+        {
+            dst[i]=src[k];
+            i++;
+        }
+    }
+
+    for(k=low; k<=high; k++) 
+        src[k]=dst[k];
+}
+
+void MergeSort::Sort(DataType &data_in){
+    src = data_in;
+    MSort(kNumThreads, src[0], src.end());
+    data_in.swap(dst);    
+}
+
+
--- src/MergeSort.hpp
+++ src/MergeSort.hpp
@@ -16,6 +16,9 @@
 /*! Basic sorting class providing the framework for benchmarking the sorting algorithms
  *  as well as the basic histogram sort.
  */
+
+typedef struct { size_t threads_remaining; size_t low; size_t high; } thread_args;
+
 class MergeSort : public SortCommon {
 
 public:
@@ -28,12 +31,14 @@ public:
 	/*!
 	  \param data_in the object getting sorted.
 	 */
-	void		Sort(DataType &data_in);
+	void  Sort(DataType &data_in);
 
 private:
-
-
-
+        DataType * src;
+        DataType dst;
+        void  *Thread_MSort(void * args);
+        void  Merge(size_t low, size_t pivot, size_t high);
+        void  MSort(size_t threads_remaining, size_t low, size_t high);
 };
 
 #endif
