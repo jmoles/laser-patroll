1015a1c56bb051acb68b93edc0233fde0a382d16
Erik J Rhodes <rhodeser@walle.ece.pdx.edu>
2013-11-26 16:07:04 -0800

    Sequential quicksort slightly modified

---
+++ src/PQuickSort.cpp
@@ -0,0 +1,49 @@
+#include "PQuickSort.hpp"
+
+
+PQuickSort::PQuickSort() {
+	
+}
+
+PQuickSort::~PQuickSort() {
+
+}
+
+
+void PQuickSort::Sort(DataType &data_in) {
+  // Call sort on the entire array (0 to size)
+  RecursiveSort(data_in, 0, data_in.size());
+}
+
+void PQuickSort::RecursiveSort(DataType &data_in, size_t left, size_t right) {
+
+  size_t i = left, j = right;
+  size_t tmp;
+  size_t pivot = data_in[(left + right) / 2];
+   
+  /* partition */
+  while (i <= j) 
+  {
+    while (data_in[i] < pivot)
+      i++;
+      
+    while (data_in[j] > pivot)
+      j--;   
+    
+    if (i <= j) 
+    {
+      tmp = data_in[i];
+      data_in[i] = data_in[j];
+      data_in[j] = tmp;
+      i++;
+      j--;
+    }
+  }
+ 
+  /* recursion */
+  if (left < j)
+    RecursiveSort(data_in, left, j);
+  
+  if (i < right)
+    RecursiveSort(data_in, i, right);
+}
---
+++ src/PQuickSort.hpp
@@ -0,0 +1,44 @@
+#ifndef QUICK_SORT_H
+
+#define QUICK_SORT_H
+
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+#include <cstdlib>
+
+#include "SortCommon.hpp"
+
+/*! Basic sorting class providing the framework for benchmarking the sorting algorithms
+ *  as well as the basic histogram sort.
+ */
+class PQuickSort : public SortCommon {
+
+public:
+
+	PQuickSort();
+
+	~PQuickSort();
+
+	//*! Runs a basic quicksort, directly modifying the passed argument.
+	/*!
+	  \param data_in the object getting sorted.
+	 */
+	void  Sort(DataType &data_in);
+
+private:
+	//*! Recursive algorithm called by "Sort".
+	/*!
+	  \param data_in the object getting sorted
+	  \param left leftmost index of the region to sort.
+	  \param right rightmost index of the region to sort.
+	 */
+	void RecursiveSort(DataType &data_in, size_t left, size_t right);
+
+
+
+};
+
+#endif
