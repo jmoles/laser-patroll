8c634a52680a231db7c60f9b9076c8433842d2bc
Eric Krause <krause.ericm@gmail.com>
2013-11-27 19:57:54 -0800

    working with vectors
    no makefile.  deal with it.
    g++ -Wall merge.cpp -o merge && ./merge

--- example/merge.cpp
+++ example/merge.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
- 
+#include <vector> 
+#include <iomanip>
 using namespace std;
  
-void merge(int*,int*,int,int,int);
-void mergesort(int *a, int*b, int low, int high)
-{
-    int pivot;
-    if(low<high)
-    {
-        pivot=(low+high)/2;
-        mergesort(a,b,low,pivot);
-        mergesort(a,b,pivot+1,high);
-        merge(a,b,low,pivot,high);
-    }
-}
-void merge(int *a, int *b, int low, int pivot, int high)
+
+void merge(std::vector<int> &a, std::vector<int> &b, int low, int pivot, int high)
 {
     int h,i,j,k;
     h=low;
@@ -53,36 +43,46 @@ void merge(int *a, int *b, int low, int pivot, int high)
     }
     for(k=low; k<=high; k++) a[k]=b[k];
 }
+
+void mergesort(std::vector<int> &a, std::vector<int> &b, int low, int high)
+{
+    int pivot;
+    if(low<high)
+    {
+        pivot=(low+high)/2;
+        mergesort(a,b,low,pivot);
+        mergesort(a,b,pivot+1,high);
+        merge(a,b,low,pivot,high);
+    }
+}
  
 int main(int argc, char* argv[])
 {    
-    int *inputarray, *outputarray;
-    
     // get length of array to generate from command line
     int length = (argc>1) ? atoi(argv[1]) : 10;
 
     // create the arrays
-    inputarray = new int[length];
-    outputarray = new int[length];
+    std::vector<int> inputarray (length);
+    std::vector<int> outputarray (length);
 
     // seed the random
-    srand(2349857);
+    srand(time(NULL));
 
     // fill up input array with randoms, echo back unsorted input
     cout<<"input:\t[";
     for (int i = 0; i < length; i++) {
         inputarray[i] = rand()%100;
-        cout<<inputarray[i]<<" ";
+        cout<<setw(2)<< inputarray[i]<<" ";
     }
     cout<<"]\n";
-  
+
     // run the mergesort
     mergesort(inputarray,outputarray,0,length-1);
 
     // display output array
     cout<<"output:\t[";
     for(int i=0; i<length; i++)
-       cout<<outputarray[i]<<" ";
+       cout<<setw(2)<<outputarray[i]<<" ";
     cout<<"]\n";
     
     // basic accuracy check
