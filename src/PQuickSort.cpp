b2af077051096b3b17bd86f6d454cfdc93e3401b
Erik J Rhodes <rhodeser@walle.ece.pdx.edu>
2013-11-28 01:52:14 -0800

    Some parallelism implemented

--- src/PQuickSort.cpp
+++ src/PQuickSort.cpp
@@ -20,23 +20,54 @@ void PQuickSort::RecursiveSort(DataType &data_in, size_t left, size_t right) {
   size_t i = left, j = right;
   size_t tmp;
   size_t pivot = data_in[(left + right) / 2];
-   
+  size_t thread_Lside, thread_Rside;
+
+
   /* partition */
   while (i <= j) 
   {
-    while (data_in[i] < pivot)
-      i++;
-      
-    while (data_in[j] > pivot)
-      j--;   
-    
-    if (i <= j) 
+    if(!kNumThreads)
     {
-      tmp = data_in[i];
-      data_in[i] = data_in[j];
-      data_in[j] = tmp;
-      i++;
-      j--;
+    // last sort, put in data and merge
+    //merge();)
+    
+    }
+  
+    else
+    {
+      kNumThreads -= 1;
+      pthread_t thread;
+      thread_Lside = thread_Rside = kNumThreads/2;
+      if (knumthreads % 2)  //if odd # of threads, randomly assign to side
+      {
+      //do srand()
+        if (rand) thread_Lside += 1;
+        else thread_Rside += 1;
+      }
+      //call sort again, with what args?
+      
+      //get necessary thread info for create
+      //put correct args in the following:
+      pthread_create(args);
+      RecursiveSort(args);
+      pthread_join(args);
+      Merge(vector1, vector2);
+
+      //actual work, put in function
+      while (data_in[i] < pivot)
+        i++;
+      
+      while (data_in[j] > pivot)
+        j--;   
+    
+      if (i <= j) 
+      {
+        tmp = data_in[i];
+        data_in[i] = data_in[j];
+        data_in[j] = tmp;
+        i++;
+        j--;
+      }
     }
   }
  
